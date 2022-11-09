1a0bec639ddadeeb646f3fde0d2034879d59b058
Valerio Luzzi <valluzzi@gmail.com>
2022-11-09 18:44:13 +0100

    FloodStyle

--- main.js
+++ main.js
@@ -1,12 +1,11 @@
-import GeoTIFF from 'ol/source/GeoTIFF';
+
+import {Raster as RasterSource, GeoTIFF} from 'ol/source'
 import Map from 'ol/Map';
-import TileLayer from 'ol/layer/WebGLTile';
+import { WebGLTile as TileLayer } from 'ol/layer'
 import View from 'ol/View';
 import OSM from 'ol/source/OSM'
 import proj4 from 'proj4';
 import {register} from 'ol/proj/proj4';
-
-
 import filetif from './COPERNICUS_EUDEM_165252.tif'
 
 //Registrare la definizione del Sistema di referimento perchè probabile che OpenLayers non la conosca
@@ -22,44 +21,58 @@ let legend = [
 ];
 
 const BlackToWhite = {
+
   color: [
     'case',
-    ['==', ['band', 2], 0],
-    '#00000000',
-    [
-    'interpolate',['linear'],
-    ['band', 1],
-    ...legend
-]],
+    ['==', ['band', 2], 0],  //se la banda2 == 0 (indica i nodata)
+    '#00000000',             //allora pixel trasparente
+    ['interpolate', ['linear'], ['band', 1], ...legend ]  //altrimenti interploazione lineare banda1 colori della legenda
+],
 }
 
+const FloodStyle = {
+  variables: {
+      level: 0,
+  },
+  color: [
+    'case',
+    ['==', ['band', 2], 0],  //se la banda2 == 0 (indica i nodata)
+    '#00000000',             //allora pixel trasparente
+    ["case",  ["<=", ["band",1], ["var", "level"]   ], "#0000ffaa", '#00000000' ]  //se la banda1 è <= livello è blu altrimenti trasparente
+],
+}
 
-const source = new GeoTIFF({
+const elevation = new GeoTIFF({
   sources: [
     {
       url: filetif,
       projection: 'EPSG:32633',
     },
   ],
-  normalize: false,
-  interpolate: false,
+  normalize: false,   //importante
+  interpolate: false, //importante
   transition: 0,
 });
+
+const osm = new TileLayer({
+  visible: true,
+  preload: Infinity,
+  source: new OSM()
+})
+
+const dem = new TileLayer({
+  source: elevation,
+  style: BlackToWhite , //FloodStyle
+})
+
 
 const map = new Map({
   target: 'map',
   layers: [
 
-    new TileLayer({
-        visible: true,
-        preload: Infinity,
-        source: new OSM()
-      }),
+      osm,
 
-      new TileLayer({
-        source: source,
-        style: BlackToWhite
-      }),
+      dem
 
 ],
   view: new View({
@@ -70,3 +83,12 @@ const map = new Map({
 
   })
 });
+
+
+const control = document.getElementById('level');
+const output  = document.getElementById('output');
+control.addEventListener('input', function () {
+  output.innerText = control.value;
+  dem.updateStyleVariables({level: parseFloat(control.value)});
+});
+output.innerText = control.value;
